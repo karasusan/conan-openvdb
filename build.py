eac93aa11d3df82b09aab8ee8091ab734e1728af
kazuki matsumoto <karasusan@gmail.com>
2018-06-30 19:29:28 +0900

    Change upload bintray repository

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="zogi", channel="testing", args="--build=missing")
+    builder = ConanMultiPackager(username="kazuki", channel="development", args="--build=missing")
     builder.add_common_builds(shared_option_name="OpenVDB:shared")
     builder.run()

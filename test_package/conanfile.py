b91884121ef7bb51ca2ecda6ff29aa71aced1a4b
kazuki matsumoto <karasusan@gmail.com>
2018-07-01 22:16:13 +0900

    fix build error

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,22 +2,16 @@ from conans.model.conan_file import ConanFile
 from conans import CMake
 import os
 
-channel = os.getenv("CONAN_CHANNEL", "testing")
-username = os.getenv("CONAN_USERNAME", "kazuki")
-
-
 class DefaultNameConan(ConanFile):
     name = "DefaultName"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
-    requires = "OpenVDB/4.0.2@%s/%s" % (username, channel)
-    default_options = "OpenVDB:shared=False"
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin")

4837e0eab4a21a800c442fb1287d235e39e2361a
kazuki matsumoto <karasusan@gmail.com>
2018-07-03 14:31:42 +0900

    exclude packages that is static library for Windows

--- build.py
+++ build.py
@@ -4,4 +4,11 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="kazuki", channel="development", args="--build=missing")
     builder.add_common_builds(shared_option_name="OpenVDB:shared")
+
+    # exclude packages that is static library for Windows
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if platform.system() != "Windows" or options["OpenVDB:shared"]:
+            filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds    
     builder.run()

3f8d7def989857e89052f02e7d87165f6588c282
kazuki matsumoto <karasusan@gmail.com>
2018-07-04 09:14:54 +0900

    exclude packages that is dynamic library for Windows

--- build.py
+++ build.py
@@ -2,13 +2,13 @@ import platform
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="kazuki", channel="development", args="--build=missing")
+    builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="OpenVDB:shared")
 
-    # exclude packages that is static library for Windows
+    # exclude packages that is dynamic library for Windows
     filtered_builds = []
     for settings, options, env_vars, build_requires, reference in builder.items:
-        if platform.system() != "Windows" or options["OpenVDB:shared"]:
+        if platform.system() != "Windows" or not options["OpenVDB:shared"]:
             filtered_builds.append([settings, options, env_vars, build_requires])
     builder.builds = filtered_builds    
     builder.run()
